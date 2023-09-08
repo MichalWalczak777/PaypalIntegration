ccc3cbd3b56a7167ed00bb4cb9752d16285e1314
Michal Walczak <michal.walczak@ex7.pl>
2023-09-08 14:23:50 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PaypalIntegration.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
